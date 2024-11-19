51b3bb81b35bd914600204ec50e775c683c14a02
Juan Useche <juanuseche47@gmail.com>
2024-11-19 01:49:07 -0300

    modificando nome

--- models/user/userModels.js
+++ models/user/userModels.js
@@ -2,7 +2,7 @@ import prisma from "../../utils/prisma.js";
 
 export const user = {
     getAll: async () => {
-        const usersAll = await prisma.user.findMany();
+        const usersAll = await prisma.User.findMany();
         return usersAll
     }
 }
